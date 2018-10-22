df0a7d8f83b24356ad2678bf43836fbba9878942
clayton <claytonwinders@gmail.com>
2018-10-22 02:03:01 +0000

    Add more example files

--- files/test1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#caeaser test
-./caesar test.txt 14
-
-./caesar en-test.txt -14
-
-diff en-en-test.txt test.txt
--- files/test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#exclusiveOr test
-./exclusiveOr test.txt testKey.txt
-
-./exclusiveOr en-test.txt testKey.txt
-
-diff en-en-test.txt test.txt
